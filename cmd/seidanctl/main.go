ad620ad80cc9f0caa723b360ceec46caa08c3167
Mark Karpeles <magicaltux@gmail.com>
2019-05-20 06:42:27 +0900

    (wip)

--- cmd/seidanctl/main.go
+++ cmd/seidanctl/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/MagicalTux/hsm"
 )
 
 func main() {
@@ -10,4 +13,23 @@ func main() {
 		fmt.Printf("Usage: %s command", os.Args[0])
 		return
 	}
+
+	h, err := hsm.New()
+	if err != nil {
+		log.Printf("failed to initialize HSM: %s", err)
+		os.Exit(1)
+	}
+
+	ks, err := h.ListKeysByName("seidan:cluster_name")
+	if err != nil {
+		log.Printf("failed to list HSM keys: %s", err)
+		os.Exit(1)
+	} else if len(ks) == 0 {
+		// Generate?
+		log.Printf("failed to list HSM keys: no keys. Please generate one.")
+		os.Exit(1)
+	}
+
+	k := ks[0]
+	log.Printf("found key: %s", k)
 }
